18009a69a52da2919b61f1d1aeaac29673567587
Gabriel Salvador Palmiciano <gabriel.palmiciano@verizon.com>
2025-08-25 13:27:49 -0300

    wip: adding android link.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import "./App.css";
 
+const HOME = 'https://palmisa.github.io/api-solutions-clear-like-web-app';
 const SHOPPING_APP_LINK = import.meta.env.VITE_APP_DEEP_LINK;
+const PROTECT_APP_LINK = import.meta.env.VITE_PROTECT_APP_ANDROID;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+
+const queryParameters = new URLSearchParams();
+
+queryParameters.append('client_id', CLIENT_ID);
+queryParameters.append('redirect_uri', HOME);
+
+const ANDROID_DEEP_LINK = `${PROTECT_APP_LINK}?${queryParameters.toString()}`
 
 function App() {
   return (
@@ -9,6 +19,8 @@ function App() {
 
       <div className="card">
         <a href={SHOPPING_APP_LINK}>Back to Shopping App.</a>
+
+        <a href={ANDROID_DEEP_LINK}>Open Protect App.</a>
       </div>
     </>
   );
